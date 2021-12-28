93c91aead520629bbb24b33209f77a61e946a6a0
Candelaria Marcote <candemarcote@gmail.com>
2021-12-27 23:18:34 -0300

    desafio 6

--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -4,18 +4,21 @@ import ItemCount from "./ItemCount"
 const productos = [
     {
         id: 1,
-        producto: 'vela',
-        precio: 365
+        title: 'vela 1',
+        precio: 365,
+        pictureUrl: 'https://ii1.pepperfry.com/media/catalog/product/h/o/568x625/hosley-lavender-fields-highly-scented-6inch-pillar-candle-hosley-lavender-fields-highly-scented-6inc-qlqxtn.jpg'
     },
     {
         id: 2,
-        producto: 'portavela',
-        precio: 850
+        title: 'vela 2',
+        precio: 850,
+        pictureUrl: 'https://hips.hearstapps.com/vader-prod.s3.amazonaws.com/1607448500-brooklinen-magic-hour-1607448496.jpg?crop=1xw:1xh;center,top&resize=480:*'
     },
     {
         id: 3,
-        producto: 'perfume',
-        precio: 400
+        title: 'vela 3',
+        precio: 400,
+        pictureUrl: 'https://assets.vogue.in/photos/5efd6cba9aeed766057e7d69/master/w_1600%2Cc_limit/kama-ayurveda-relaxing-candles-aromatherapy-scented-candles.jpg'
     }
 ]
 
