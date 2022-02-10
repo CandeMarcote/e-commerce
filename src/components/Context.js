69e210aab2dc4113011f707de72e638a99ac0f28
Candelaria Marcote <candemarcote@gmail.com>
2022-02-10 19:38:33 -0300

    funciona el localStorage

--- src/components/Context.js
+++ src/components/Context.js
@@ -1,4 +1,5 @@
 import { createContext, useState, useContext } from 'react';
+import { useLocalStorage } from './useLocalStorage';
 
 const Context = createContext([])
 
@@ -6,16 +7,16 @@ export const UseCartContext = () => useContext(Context);
 
 const CartProvider = ({children}) => {
 
-    const [cart, setCart] = useState([])
+    const [cart, setCart] = useLocalStorage('cart', '')
     const [precio_total, setPrecio_total] = useState(0)
     const [cantidad_total, setCantidad_total] = useState(0)
 
+    
 
     const addProduct = (item, cantidad) => {
         
         const index = cart.findIndex((i) => i.item.id === item.id);
         setCart([...cart, {item, cantidad}])
-        
 
         if (index > -1) {
             cart.splice(index, 1);
@@ -45,7 +46,7 @@ const CartProvider = ({children}) => {
        if(cantidad > 1) { const itemFiltrado = cart.findIndex((i)=> i.item.id == item.id)
 
         cart.splice(itemFiltrado, 1/*, item*/);
-
+        
         setCart([...cart, {item, cantidad: cantidad - 1}])}
      
     };
